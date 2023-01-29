a6ff0ada3b7990e40dae9c4c3ca851acff28dfc9
AlexanderLevkin <alexanlevkin@gmail.com>
2023-01-29 19:03:54 +0300

    add POST GET PUT methods

---
+++ tests/test_google_maps_api.py
@@ -0,0 +1,24 @@
+from requests import Response
+
+from utils.api import Google_maps_api
+
+"""Создание изменения и удаление новой локации"""
+
+
+class Test_create_place:
+
+    def test_create_new_place(self):
+        print("\nМетод POST")
+        result_post: Response = Google_maps_api.create_new_place()
+        check_post = result_post.json()
+        place_id = check_post.get("place_id")
+        print(place_id)
+
+        print("\nМетод GET POST")
+        result_get: Response = Google_maps_api.get_new_place(place_id=place_id)
+
+        print("\nМетод PUT")
+        result_put: Response = Google_maps_api.put_new_place(place_id=place_id)
+
+        print("\nМетод GET PUT")
+        result_get: Response = Google_maps_api.get_new_place(place_id=place_id)
